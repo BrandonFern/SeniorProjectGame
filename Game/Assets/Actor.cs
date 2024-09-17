44cdcf1349aadafead889947b498ad822cb7443d
BrandonFern <fernandezbrand081@gmail.com>
2024-09-17 10:11:52 -0500

    New textures and enemies react to being hit

--- Game/Assets/Actor.cs
+++ Game/Assets/Actor.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Actor : MonoBehaviour
 {
     int currentHealth;
     public int maxHealth = 3;
+    private NavMeshAgent agent;
 
     void Awake()
     {
         currentHealth = maxHealth;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     public void TakeDamage(int amount)
@@ -18,6 +21,19 @@ public class Actor : MonoBehaviour
 
         if(currentHealth <= 0)
         { Death(); }
+        else
+        {
+            StartCoroutine(ApplyKnockbackEffect());
+        }
+    }
+    IEnumerator ApplyKnockbackEffect()
+    {
+        float originalSpeed = agent.speed;
+        agent.speed = 0;
+
+        yield return new WaitForSeconds(1f);
+
+        agent.speed = originalSpeed;
     }
 
     void Death()
