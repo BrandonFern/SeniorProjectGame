7ab1f96197ca21e3efb46ad5d32425234850a279
BrandonFern <fernandezbrand081@gmail.com>
2024-09-25 14:16:59 -0500

    Log in scene works

---
+++ Game/Assets/Scripts/AuthManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e43df2b9593f947caaa5e36ef918a943df39f4d7290809d36ceec32c05830527
+size 6149
---
+++ Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01bba2db58d53824fe0466abe61da2f9b975b64c05f901777bdb44518f31794a
+size 873
