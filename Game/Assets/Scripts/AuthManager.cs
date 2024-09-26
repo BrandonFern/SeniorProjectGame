61879ff9c1dad3fb2127afaaf55549b6ece1f000
BrandonFern <fernandezbrand081@gmail.com>
2024-09-25 20:10:24 -0500

    Loging in now fetches your player data and puts it into a JSON file for later use

--- Game/Assets/Scripts/AuthManager.cs
+++ Game/Assets/Scripts/AuthManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e43df2b9593f947caaa5e36ef918a943df39f4d7290809d36ceec32c05830527
-size 6149
+oid sha256:84a9dc7713c60112c8229ace33912a5fb8665c9280a79652a224bd1ffe917c0a
+size 10999
