90a6b0de45f872f855aadaffb1c5a47155fd5bcd
BrandonFern <fernandezbrand081@gmail.com>
2024-09-27 18:24:41 -0500

    Logging in pops up a button to go to game. WIll be replaced to go to main menu THEN game

--- Game/Assets/Scripts/Firebasecheck.cs
+++ Game/Assets/Scripts/Firebasecheck.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c2224d5f382f6b5fbbbced7d9fb0d7ac2c79faeb17d3565d6afd7e4840c380a
-size 371
+oid sha256:3fb8a59e8abef05dc69f81018fb0f16917a957486735d7fc8bbda4632fa0f4f0
+size 438
---
+++ Game/Assets/Scripts/LoggedManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3bb2d0e3aa73bae911e51cc20b18e21fbc810e3e230ade4ecb4d740fba85fc4
+size 945
