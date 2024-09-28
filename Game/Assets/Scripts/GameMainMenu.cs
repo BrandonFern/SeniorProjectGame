8794b4eb0844a8019fccbe379339505c3736bccc
Rolando Pequeno <rolando.pequeno01@utrgv.edu>
2024-09-28 16:20:19 -0500

    can now go to game scene from game menu

--- Game/Assets/Scripts/GameMainMenu.cs
+++ Game/Assets/Scripts/GameMainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6ba0ec102a3f972d7bbede00a765266e7383edf1bd1fa64f265f64b46df2be3
-size 418
+oid sha256:0675ca1ee405aaec621a09a483f2c816133a313adb07845322d8ff2e518514a1
+size 422
