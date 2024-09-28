b13b7f46e52b73088bebdfa3b022ef01fb945406
Rolando Pequeno <rolando.pequeno01@utrgv.edu>
2024-09-28 16:04:33 -0500

    add back button on setting

--- Game/Assets/Scripts/GameMainMenu.cs
+++ Game/Assets/Scripts/GameMainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a5dc0a4585deaceca4a9ce7ba97106f34a56fcb28b2ebf11476a3d1ca3fa03e
-size 421
+oid sha256:b6ba0ec102a3f972d7bbede00a765266e7383edf1bd1fa64f265f64b46df2be3
+size 418
