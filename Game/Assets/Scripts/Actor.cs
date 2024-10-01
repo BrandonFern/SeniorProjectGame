5bc97e4aaa4e243d2f9d785e9a3579766e73d27c
BrandonFern <fernandezbrand081@gmail.com>
2024-10-01 18:59:51 -0500

    When an enemy dies, it will call the playerHealth script and from there do a check to see if there was an upgrade in level. If there was, then our max health and current health are scaled appropiately in both value and healthbar. Also added stationary enemies to test out level upgrades quickly. Includes text file as an example for gathering Json data

--- Game/Assets/Scripts/Actor.cs
+++ Game/Assets/Scripts/Actor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1e9ed84bf12c2461d831bbbce0d4bd2cfb2befb018f5e95bb5a40492a203bb7
-size 1819
+oid sha256:b17e5c1ecc435047435f53f0725bd5cf3e1b5e4ba8c30e7faf92cb02f72759db
+size 2950
--- Game/Assets/Scripts/PlayerHealth.cs
+++ Game/Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c64e5ac101d73ec21deac0b9733dc5a097be501d0fddb607372cfe4240abb078
-size 3126
+oid sha256:8657a4e7e0eaf086928b7ab22260ba5481651766788e1423b7dd3ad5d99de2eb
+size 3987
