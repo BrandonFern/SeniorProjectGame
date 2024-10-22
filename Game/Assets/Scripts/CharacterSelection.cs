d4257f3ba12f16011d474f527dfd36edae94aac6
Rolando Pequeno <rolando.pequeno01@utrgv.edu>
2024-10-22 15:01:49 -0500

    animtaion

--- Game/Assets/Scripts/CharacterSelection.cs
+++ Game/Assets/Scripts/CharacterSelection.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2acc95360246e702226fb68e958ae921eff870854267fcfd054b9cf7541c236
-size 1072
+oid sha256:cbbe2b2ffa44a126c787d184d55f55b62f8f2c8a181c8dc5e0e61802c150061f
+size 1519
--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb545bfe52b90a68730465f46d5cac3528f6b27f3e2ef9483cc8e45cb74c280
-size 9935
+oid sha256:2ad6bcc083d262f2fa71ca6782bda28add4682e49b5a022aa7b20824c4ee731c
+size 10393
