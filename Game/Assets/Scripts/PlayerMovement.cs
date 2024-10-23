6fabead791b2bdf7e0b15dbc8a3de34bde73404a
BrandonFern <fernandezbrand081@gmail.com>
2024-10-23 17:46:25 -0500

    Player has animations

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb545bfe52b90a68730465f46d5cac3528f6b27f3e2ef9483cc8e45cb74c280
-size 9935
+oid sha256:57558ddbdf0b7ff3e28c4fe666d653bdc970be3208c1fc3bfb884523e07ec1c8
+size 10831
