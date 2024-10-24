c57d6c44faa62358ed6d320acf7ab26025371e1b
BrandonFern <fernandezbrand081@gmail.com>
2024-10-24 18:11:32 -0500

    Enemy animations

--- Game/Assets/Scripts/EnemyConeDetection.cs
+++ Game/Assets/Scripts/EnemyConeDetection.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e69442f70508a3feb882c6b8b65ba83f8d24ccf3a1212bac4c14468b0f7c414
-size 4295
+oid sha256:d20a531e8cfbcab4cf346127064dc54ecb7f56fba55569f00aa67df120856944
+size 4691
