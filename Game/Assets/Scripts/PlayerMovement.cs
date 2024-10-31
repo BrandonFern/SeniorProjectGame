e2374ee165482255b0c89bb0dc5200d31cb03c12
BrandonFern <fernandezbrand081@gmail.com>
2024-10-31 16:02:38 -0500

    Added new animations to the player controller, tweaked movement script trigger animations, tweaked enemy and player sword position in hand, and added mesh collider to enemy sword

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57558ddbdf0b7ff3e28c4fe666d653bdc970be3208c1fc3bfb884523e07ec1c8
-size 10831
+oid sha256:021336cf25868850a370dbcd52bdb1bfe4496f55e4de3fd986ca5420dbc39092
+size 11060
