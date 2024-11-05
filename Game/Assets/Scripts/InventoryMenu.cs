a10170e55c34641e1d040a6854533c68b267b0d3
BrandonFern <fernandezbrand081@gmail.com>
2024-11-04 18:14:15 -0600

    Starting on player assistance stats based on KDR ratio. Only applied to the first level so far. Depending on ratio, adds either 2 or 3 to the value of dmg, health, and speed.

--- Game/Assets/Scripts/InventoryMenu.cs
+++ Game/Assets/Scripts/InventoryMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df6fcc9637853a443c5445d1ab7eec121d17789cf2fe3a2d89a8afc7b41979ca
-size 1895
+oid sha256:2ffcba90ee77e3a5c6564de5bd6cac2f4daad95fe11dccdd5961a5ceca8ead04
+size 1897
---
+++ Game/Assets/Scripts/PlayerAssist.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b18a30c445d6f72ff3a57cb63924a36b501fca98d2f1dd5259e581328713feb
+size 2968
