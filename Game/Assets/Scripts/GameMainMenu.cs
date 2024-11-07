d7f8138e3a42e74ad9453c3be81075af1d9fc145
erickm03 <erick.martinez03@utrgv.edu>
2024-11-07 16:25:14 -0600

    Map Selection Update
    Added Level 3 option to the selections. Cleaned up selection code.
    Added enemy model to the front of the screen to view what enemy is in the map.

--- Game/Assets/Scripts/GameMainMenu.cs
+++ Game/Assets/Scripts/GameMainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c64093035343189997a077556cc3fd7d535f60dd74a03205348db08bde2be2e
-size 502
+oid sha256:674b4dd2083648ed69b4e7e1b6abe4792eecd1dd26615afa779c4579372b39a6
+size 583
