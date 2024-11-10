e56a1f39a0e014b54d2a0efa4c6135582a513274
BrandonFern <fernandezbrand081@gmail.com>
2024-11-09 19:29:17 -0600

    Scripts for chests that will give the player a random item. Rarity can be adjusted per level and item gets announced on UI and added to the JSON file for database upload. Needs: integration into Inventory menu, association with player stats(damage,speed,health,reach), and way to select them in game

---
+++ Game/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:526cb32bbda103c80743771775036ff69308aced5e2a90dccf1df1efc9726837
+size 1995
---
+++ Game/Assets/Scripts/RandomItemGenerator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b978ac36bdcdbbef397a36c913531dfe91a932b0a74f23dc270f7afe3deda9d5
+size 1897
