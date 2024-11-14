8aae8270a6c51404ecc617e021cb7043c9fbec37
erickm03 <erick.martinez03@utrgv.edu>
2024-11-14 16:30:16 -0600

    Menu Bug Fix
    Cleaned up the PauseMenuManager, removed the function that was confirming settings changes.

--- Game/Assets/Scripts/PauseMenuManager.cs
+++ Game/Assets/Scripts/PauseMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f45b12ff3dd66268f5e376c7f9bd8d5b91cda661aa201ce5831122045cbc3a08
-size 4549
+oid sha256:768fa5849a351ca801e376f191fa6972baaf944cf4c30df4d145ff17408c4013
+size 3963
