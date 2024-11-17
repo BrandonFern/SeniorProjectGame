4af3946034a0f9217b7fe4162c767ee55770da72
BrandonFern <fernandezbrand081@gmail.com>
2024-11-17 13:57:50 -0600

    New levels added to main menu select screen. Player model shown on screen and changes based on your player level

--- Game/Assets/Scripts/GameMainMenu.cs
+++ Game/Assets/Scripts/GameMainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:674b4dd2083648ed69b4e7e1b6abe4792eecd1dd26615afa779c4579372b39a6
-size 583
+oid sha256:993e71456b7c4f37903320f092c76204cd9c9575f1d2db0828bbc5f19982738e
+size 745
